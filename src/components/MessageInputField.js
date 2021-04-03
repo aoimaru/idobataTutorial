fb5eff67a29961ffe2d32b321d717c9af8bb4e29
中村碧海 <nakamurahekikai@nakamurahekikainoMacBook-Pro-2.local>
2021-04-04 02:55:10 +0900

    generate avatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { Avatar, Grid } from '@material-ui/core';
-
+import { gravatarPath } from '../gravatar';
 
 
 const useStyles = makeStyles({
@@ -12,13 +12,14 @@ const useStyles = makeStyles({
     },
 })
 
-const MessageInputFeild = () => {
+const MessageInputFeild = ({ name }) => {
     const classes = useStyles();
+    const avatarPath = gravatarPath(name);
     return (
         <div className={classes.root}>
             <Grid container> 
                 <Grid xs={1}>
-                    <Avatar />
+                    <Avatar src={avatarPath}/>
                 </Grid>
                 <Grid xs={10}>
                     入力
