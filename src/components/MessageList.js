ad5ba62948428a869bc12134ab7cc51fec006e62
中村碧海 <nakamurahekikai@nakamurahekikainoMacBook-Pro-2.local>
2021-04-04 22:12:08 +0900

    display messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
+import { List } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { messagesRef } from '../firebase';
+
+import MessageItem from './MessageItem';
 
 
 
 const useStyles = makeStyles({
     root: {
         gridRow: 1,
+        overflow: 'auto',
+        width: '100%',
     },
 })
 
@@ -17,7 +22,7 @@ const MessageList = () => {
     const classes = useStyles();
 
     useEffect(() => {
-        messagesRef.orderByKey().limitToLast(3).on('value', (snapshot) => {
+        messagesRef.orderByKey().limitToLast(10).on('value', (snapshot) => {
             const messages = snapshot.val();
             if(messages === null) return;
             const entries = Object.entries(messages);
@@ -30,7 +35,15 @@ const MessageList = () => {
             setMessages(newMessage);
         });
     }, []);
-    return <div className={classes.root} >MessageList</div>;
+    return (
+        <List className={classes.root} >
+            {
+                messages.map(({ key, name, text }) =>{
+                    return <MessageItem key={key} name={name} text={text} >item</MessageItem>;
+                })
+            }
+        </List>
+    );
 
     
     
